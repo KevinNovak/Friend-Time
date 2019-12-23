de3b66c8a8b13f2b72386f3d3570051804f6bdb5
Kevin Novak <kevinmnovak@yahoo.com>
2019-12-22 23:44:08 -0500

    Update database script

--- scripts/create-database.sql
+++ scripts/create-database.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.6deb5
+-- version 4.9.2
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Aug 24, 2019 at 05:24 PM
--- Server version: 10.3.15-MariaDB-1
--- PHP Version: 7.3.4-2
+-- Host: localhost
+-- Generation Time: Dec 23, 2019 at 04:43 AM
+-- Server version: 10.3.18-MariaDB-0+deb10u1
+-- PHP Version: 7.3.11-1~deb10u1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -77,6 +79,8 @@ ALTER TABLE `Member`
 --
 ALTER TABLE `Member`
   MODIFY `MemberId` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

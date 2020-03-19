f8b71acf5b9cd411b3ea43bc2c3a5969a01bede2
Kevin Novak <kevinmnovak@yahoo.com>
2020-03-19 13:50:47 -0400

    Fix name

--- src/services/sites/discord-bot-list-com-site.ts
+++ src/services/sites/discord-bot-list-com-site.ts
@@ -4,7 +4,7 @@ import { BotSite } from './bot-site';
 
 export class DiscordBotListComSite implements BotSite {
     public enabled = false;
-    public name = 'bots.ondiscord.xyz';
+    public name = 'discordbotlist.com';
 
     constructor(private config: DiscordBotListComConfig, private httpService: HttpService) {
         this.enabled = this.config.enabled;

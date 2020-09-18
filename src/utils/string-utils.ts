169aa80726fc0477e270722ef3a373b1634996b2
Kevin Novak <kevinmnovak@yahoo.com>
2020-09-18 19:36:57 -0400

    Use "this" keyword instead of class name

--- src/utils/string-utils.ts
+++ src/utils/string-utils.ts
@@ -2,7 +2,7 @@ export abstract class StringUtils {
     private static QUOTE_MAX_LENGTH = 200;
 
     public static formatQuote(quote: string): string {
-        return StringUtils.truncate(quote.replace('\n', ' '), this.QUOTE_MAX_LENGTH);
+        return this.truncate(quote.replace('\n', ' '), this.QUOTE_MAX_LENGTH);
     }
 
     public static formatMention(id: string): string {

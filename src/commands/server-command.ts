d0b126715ed42b284a275afa81758067e8ebda47
Kevin Novak <kevinmichaelnovak@gmail.com>
2021-10-25 00:47:44 -0400

    Implement server command

--- src/commands/server-command.ts
+++ src/commands/server-command.ts
@@ -76,93 +76,87 @@ export class ServerCommand implements Command {
     constructor(private settingManager: SettingManager) {}
 
     public async execute(intr: CommandInteraction, data: EventData): Promise<void> {
-        return;
+        if (!data.guild) {
+            data.guild = new GuildData();
+            data.guild.discordId = intr.guild.id;
+        }
+
+        switch (intr.options.getSubcommand()) {
+            case Lang.getCom('subCommands.view'): {
+                let settingList = this.settingManager.list(data.guild, data.lang());
+                await MessageUtils.sendIntr(
+                    intr,
+                    Lang.getEmbed('displayEmbeds.settingsServer', data.lang(), {
+                        SETTING_LIST: settingList,
+                        SERVER_ID: intr.guild.id,
+                    }).setAuthor(intr.guild.name, intr.guild.iconURL())
+                );
+                return;
+            }
+            case Lang.getCom('subCommands.edit'): {
+                // Find setting to configure
+                let settingInput = intr.options.getString(Lang.getCom('arguments.setting'));
+                let setting = this.settingManager.find(settingInput);
+                if (!setting) {
+                    await MessageUtils.sendIntr(
+                        intr,
+                        Lang.getEmbed('validationEmbeds.notFoundSetting', data.lang())
+                    );
+                    return;
+                }
+
+                // Remove setting value
+                let reset = intr.options.getBoolean(Lang.getCom('arguments.reset')) ?? false;
+                if (reset) {
+                    setting.clear(data.guild);
+                    await data.guild.save();
+
+                    await MessageUtils.sendIntr(
+                        intr,
+                        Lang.getEmbed('resultEmbeds.removedSettingGuild', data.lang(), {
+                            SETTING_NAME: setting.displayName(data.lang()),
+                        })
+                    );
+                    return;
+                }
+
+                // Set setting value
+                let value = await setting.retrieve(intr, data);
+                if (value == null) {
+                    return;
+                }
+
+                setting.apply(data.guild, value);
+                await data.guild.save();
+
+                await MessageUtils.sendIntr(
+                    intr,
+                    Lang.getEmbed('resultEmbeds.updatedSettingGuild', data.lang(), {
+                        SETTING_NAME: setting.displayName(data.lang()),
+                        SETTING_VALUE: setting.valueDisplayName(value, data.lang()),
+                    })
+                );
+                return;
+            }
+            case Lang.getCom('subCommands.remove'): {
+                this.settingManager.settings.forEach(setting => setting.clear(data.guild));
+                // Remove guild's bots
+                if (data.guild.bots) {
+                    await GuildBotData.delete(data.guild.bots.map(botData => botData.id));
+                }
+                // Remove guild's list items
+                if (data.guild.listItems) {
+                    await GuildListItemData.delete(
+                        data.guild.listItems.map(listItemData => listItemData.id)
+                    );
+                }
+                await data.guild.save();
+                await MessageUtils.sendIntr(
+                    intr,
+                    Lang.getEmbed('resultEmbeds.removedGuild', data.lang())
+                );
+                return;
+            }
+        }
     }
-
-    // public async execute(intr: CommandInteraction, data: EventData): Promise<void> {
-    //     if (!data.guild) {
-    //         data.guild = new GuildData();
-    //         data.guild.discordId = intr.guild.id;
-    //     }
-
-    //     // Display settings
-    //     if (args.length === 2) {
-    //         let settingList = this.settingManager.list(data.guild, data.lang());
-    //         await MessageUtils.sendIntr(
-    //             intr,
-    //             Lang.getEmbed('displayEmbeds.settingsServer', data.lang(), {
-    //                 SETTING_LIST: settingList,
-    //                 SERVER_ID: intr.guild.id,
-    //             }).setAuthor(intr.guild.name, intr.guild.iconURL())
-    //         );
-    //         return;
-    //     }
-
-    //     if (args.length > 2) {
-    //         // Remove all setting data
-    //         // TODO: Move to method
-    //         if (args[2].toLowerCase() === Lang.getCom('commands.remove')) {
-    //             this.settingManager.settings.forEach(setting => setting.clear(data.guild));
-    //             // Remove guild's bots
-    //             if (data.guild.bots) {
-    //                 await GuildBotData.delete(data.guild.bots.map(botData => botData.id));
-    //             }
-    //             // Remove guild's list items
-    //             if (data.guild.listItems) {
-    //                 await GuildListItemData.delete(
-    //                     data.guild.listItems.map(listItemData => listItemData.id)
-    //                 );
-    //             }
-    //             await data.guild.save();
-    //             await MessageUtils.sendIntr(
-    //                 intr,
-    //                 Lang.getEmbed('resultEmbeds.removedGuild', data.lang())
-    //             );
-    //             return;
-    //         }
-
-    //         // Find setting to configure
-    //         let setting = this.settingManager.find(args[2]);
-
-    //         // No setting found
-    //         if (!setting) {
-    //             await MessageUtils.sendIntr(
-    //                 intr,
-    //                 Lang.getEmbed('validationEmbeds.notFoundSetting', data.lang())
-    //             );
-    //             return;
-    //         }
-
-    //         // Remove setting value
-    //         if (args.length > 3 && args[3].toLowerCase() === Lang.getCom('commands.remove')) {
-    //             setting.clear(data.guild);
-    //             await data.guild.save();
-
-    //             await MessageUtils.sendIntr(
-    //                 intr,
-    //                 Lang.getEmbed('resultEmbeds.removedSettingGuild', data.lang(), {
-    //                     SETTING_NAME: setting.displayName(data.lang()),
-    //                 })
-    //             );
-    //             return;
-    //         }
-
-    //         // Set setting value
-    //         let value = await setting.retrieve(intr, data);
-    //         if (value == null) {
-    //             return;
-    //         }
-
-    //         setting.apply(data.guild, value);
-    //         await data.guild.save();
-
-    //         await MessageUtils.sendIntr(
-    //             intr,
-    //             Lang.getEmbed('resultEmbeds.updatedSettingGuild', data.lang(), {
-    //                 SETTING_NAME: setting.displayName(data.lang()),
-    //                 SETTING_VALUE: setting.valueDisplayName(value, data.lang()),
-    //             })
-    //         );
-    //     }
-    // }
 }

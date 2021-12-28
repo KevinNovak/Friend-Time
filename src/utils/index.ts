1a8ceccaf509cd39ab2e55013322dc86a17cdedc
Kevin Novak <kevinmichaelnovak@gmail.com>
2021-12-27 22:14:19 -0500

    Create CommandUtils#runChecks() for validation

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,11 +1,12 @@
 export { ClientUtils } from './client-utils';
 export { CollectorUtils } from './collector-utils';
+export { CommandUtils } from './command-utils';
 export { DataUtils } from './data-utils';
 export { EmbedUtils } from './embed-utils';
 export { FormatUtils } from './format-utils';
-export { PartialUtils } from './partial-utils';
 export { MathUtils } from './math-utils';
 export { MessageUtils } from './message-utils';
+export { PartialUtils } from './partial-utils';
 export { PermissionUtils } from './permission-utils';
 export { RegexUtils } from './regex-utils';
 export { ShardUtils } from './shard-utils';

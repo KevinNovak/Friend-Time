f16a6ef5b19ef7dcfbb0467b45cb7ad225a616a8
Kevin Novak <kevinmichaelnovak@gmail.com>
2022-01-08 16:20:39 -0500

    Add command cooldowns

--- src/utils/format-utils.ts
+++ src/utils/format-utils.ts
@@ -1,5 +1,5 @@
 import { Guild } from 'discord.js';
-import { DateTime } from 'luxon';
+import { DateTime, Duration } from 'luxon'; // TODO: Missing types
 
 import { LangCode, Language, TimeFormat, TimeFormatOption } from '../models/enums';
 
@@ -48,5 +48,26 @@ export class FormatUtils {
         let locale = Language.locale(langCode);
         dateTime = dateTime.setLocale(locale);
         return dateTime.toFormat(TimeFormat.Data[timeFormat].format);
+    }
+
+    public static duration(milliseconds: number, langCode: LangCode): string {
+        return Duration.fromObject(
+            Object.fromEntries(
+                Object.entries(
+                    Duration.fromMillis(milliseconds, { locale: Language.locale(langCode) })
+                        .shiftTo(
+                            'year',
+                            'quarter',
+                            'month',
+                            'week',
+                            'day',
+                            'hour',
+                            'minute',
+                            'second'
+                        )
+                        .toObject()
+                ).filter(([_, value]) => !!value) // Remove units that are 0
+            )
+        ).toHuman({ maximumFractionDigits: 0 });
     }
 }

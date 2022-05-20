49aa7cf9978ed2f5e05f2294f1ad073662fc9c23
Kevin Novak <kevinmichaelnovak@gmail.com>
2022-05-20 17:43:11 -0400

    Use v10 of the REST API

--- src/commands/time-command.ts
+++ src/commands/time-command.ts
@@ -1,4 +1,4 @@
-import { ApplicationCommandOptionType } from 'discord-api-types/v9';
+import { ApplicationCommandOptionType } from 'discord-api-types/v10';
 import { ChatInputApplicationCommandData, CommandInteraction, PermissionString } from 'discord.js';
 import { createRequire } from 'node:module';
 

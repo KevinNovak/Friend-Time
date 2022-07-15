18279359569ec2816f1fd56559ae2c4d2de8e26f
Kevin Novak <kevinmichaelnovak@gmail.com>
2022-07-15 00:01:46 -0400

    Fix sorting

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -12,6 +12,6 @@ export { PermissionUtils } from './permission-utils.js';
 export { RegexUtils } from './regex-utils.js';
 export { ShardUtils } from './shard-utils.js';
 export { StringUtils } from './string-utils.js';
+export { ThreadUtils } from './thread-utils.js';
 export { TimeUtils } from './time-utils.js';
 export { TimeZoneUtils } from './time-zone-utils.js';
-export { ThreadUtils } from './thread-utils.js';
